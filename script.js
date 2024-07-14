7c368505ddff45339cc124304ad741654a6bcb71
lucas4556 <161216307+lucas4556@users.noreply.github.com>
2024-07-14 10:02:40 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-if(localStorage.getItem('visitCount')){
-    var visitCount = parseInt(localStorage.getItem('visitCount')) + 1
-} else {
-    var visitCount = 1
-}
-
-localStorage.setItem('visitCount', visitCount)
-
-document.getElementById('visitCount').textContent = visitCount
